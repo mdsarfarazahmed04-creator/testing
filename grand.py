f9547f9ee1eaca1340927cddf527747b55dc52b1
unknown <mdsarfarazahmed04@gmail.com>
2025-10-14 22:52:57 +0530

    updated information

---
+++ grand.py
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
